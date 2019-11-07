f21801c3c6d3afdbb2cdd45cee397f2bf6afc1e4
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-11-07 13:17:53 -0500

    add MultiSerializerViewSetMixin to handle multiple serializers per view

---
+++ src/shipchain_common/views.py
@@ -0,0 +1,63 @@
+"""
+Copyright 2019 ShipChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+class MultiSerializerViewSetMixin:
+    def get_serializer_class(self):
+        """
+        Look for serializer class in self.serializer_action_classes, which
+        should be a dict mapping action name (key) to serializer class (value),
+        i.e.:
+
+        class MyViewSet(MultiSerializerViewSetMixin, ViewSet):
+            serializer_class = MyDefaultSerializer
+            serializer_action_classes = {
+               'list': MyListSerializer,
+               'my_action': MyActionSerializer,
+            }
+
+            @action
+            def my_action:
+                ...
+
+        If there's no entry for that action then just fallback to the regular
+        get_serializer_class lookup: self.serializer_class, DefaultSerializer.
+
+        Built-in actions:
+            create
+            retrieve
+            update         PUT
+            partial_update PATCH
+            destroy
+            list
+        """
+        action = self.action
+
+        try:
+            # PUT and PATCH should use the same serializers if not separately defined
+            if action == 'partial_update' and 'partial_update' not in self.serializer_action_classes:
+                action = 'update'
+
+            if 'format' in self.kwargs:
+                try:
+                    return self.serializer_action_classes[f'{action}.{self.kwargs["format"]}']
+                except (KeyError, AttributeError):
+                    pass
+
+            return self.serializer_action_classes[action]
+
+        except (KeyError, AttributeError):
+            return super(MultiSerializerViewSetMixin, self).get_serializer_class()
