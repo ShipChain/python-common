e29b32b0f3297309c8949635a14ff948dcc0d970
Adam Hodges <ahodges@shipchain.io>
2019-12-12 15:39:45 -0500

    Add NullableFieldsMixin (for serializers)

---
+++ src/shipchain_common/serializers.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+from rest_framework.fields import SkipField
+
+
+class NullableFieldsMixin:
+    def to_representation(self, instance):
+        # Remove null fields from serialized object
+        ret = OrderedDict()
+        fields = [field for field in self.fields.values() if not field.write_only]
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is not None:
+                representation = field.to_representation(attribute)
+                if representation is None:
+                    # Do not serialize empty objects
+                    continue
+                if isinstance(representation, list) and not representation:
+                    # Do not serialize empty lists
+                    continue
+                ret[field.field_name] = representation
+
+        return ret
