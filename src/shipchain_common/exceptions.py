c6afc13778f35309b8fca2a3c8cb37c5b63328d6
James Neyer <jamesneyer@gmail.com>
2020-03-09 16:11:42 -0400

    Add URLShortenerError

--- src/shipchain_common/exceptions.py
+++ src/shipchain_common/exceptions.py
@@ -212,3 +212,16 @@ class RPCError(APIException):
 
         if status_code:
             self.status_code = status_code
+
+
+class URLShortenerError(APIException):
+    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+    default_detail = 'URL Shortener Error.'
+    default_code = 'server_error'
+
+    def __init__(self, detail, status_code=None, code=None):
+        super().__init__(detail, code)
+        self.detail = detail
+
+        if status_code:
+            self.status_code = status_code
