5b21583430ea31efed17ae58abdcafc32bb67036
Adam Hodges <ahodges@shipchain.io>
2020-09-23 14:17:42 -0400

    Add last_request method to httpretty wrapper

--- src/shipchain_common/test_utils/httpretty_asserter.py
+++ src/shipchain_common/test_utils/httpretty_asserter.py
@@ -47,6 +47,9 @@ class ResponsesHTTPrettyWrapper:
     def latest_requests(self):
         return self.mock.calls
 
+    def last_request(self):
+        return self.mock.calls[-1] if self.mock.calls else None
+
     def reset(self):
         self.mock._calls.reset()  # pylint:disable=protected-access
 
