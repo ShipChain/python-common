3a44e6673defbd570218c7901fd71315fcaf17a7
James Neyer <jamesneyer@gmail.com>
2020-10-26 15:40:36 -0400

    Use same functionality as in upstream

--- src/shipchain_common/viewsets.py
+++ src/shipchain_common/viewsets.py
@@ -232,9 +232,7 @@ class ConfigurableGenericViewSet(GenericViewSet):
         """
         serialization_type = kwargs.pop('serialization_type', mixins.SerializationType.REQUEST)
         serializer_class = self.get_serializer_class(serialization_type)
-        if 'context' not in kwargs:
-            kwargs['context'] = {}
-        kwargs['context'].update(self.get_serializer_context())
+        kwargs.setdefault('context', self.get_serializer_context())
         return serializer_class(*args, **kwargs)
 
     # pylint: disable=arguments-differ
